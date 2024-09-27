4f2de35fdd643d189d462b8a3060b240310ab6d3
Oleksandra Koshyk <sakoshik1984@gmail.com>
2024-09-27 19:10:32 +0200

    New deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
